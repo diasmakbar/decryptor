eade0a218899b3b976e0e59bca0333d0d2336838
dias <dias.akbar@jalin.co.id>
2025-09-19 17:28:23 +0700

    change to decrypto

--- game-utils.js
+++ game-utils.js
@@ -4,15 +4,15 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-database.js";
 import { db } from "./firebase.js";
 
-export const gameRef         = (gameId)         => ref(db, `games/${gameId}`);
-export const roundsRef       = (gameId)         => ref(db, `games/${gameId}/rounds`);
-export const roundRef        = (gameId, r)      => ref(db, `games/${gameId}/rounds/${r}`);
-export const codeRef         = (gameId, r)      => ref(db, `games/${gameId}/rounds/${r}/code`);
-export const teamsRef        = (gameId)         => ref(db, `games/${gameId}/teams`);
-export const teamRef         = (gameId, team)   => ref(db, `games/${gameId}/teams/${team}`);
-export const currentRoundRef = (gameId)         => ref(db, `games/${gameId}/currentRound`);
-export const winnerRef       = (gameId)         => ref(db, `games/${gameId}/winner`);
-export const teamOrderRef    = (gameId)         => ref(db, `games/${gameId}/teamOrder`);
+export const gameRef         = (gameId)         => ref(db, `games/decrypto/${gameId}`);
+export const roundsRef       = (gameId)         => ref(db, `games/decrypto/${gameId}/rounds`);
+export const roundRef        = (gameId, r)      => ref(db, `games/decrypto/${gameId}/rounds/${r}`);
+export const codeRef         = (gameId, r)      => ref(db, `games/decrypto/${gameId}/rounds/${r}/code`);
+export const teamsRef        = (gameId)         => ref(db, `games/decrypto/${gameId}/teams`);
+export const teamRef         = (gameId, team)   => ref(db, `games/decrypto/${gameId}/teams/${team}`);
+export const currentRoundRef = (gameId)         => ref(db, `games/decrypto/${gameId}/currentRound`);
+export const winnerRef       = (gameId)         => ref(db, `games/decrypto/${gameId}/winner`);
+export const teamOrderRef    = (gameId)         => ref(db, `games/decrypto/${gameId}/teamOrder`);
 
 export function getHostTeam(teamOrder, round) {
   if (!Array.isArray(teamOrder) || teamOrder.length < 2) return teamOrder?.[0] || null;
@@ -57,9 +57,9 @@ export function mountWinnerBanner(gameId) {
 
 export async function advanceRoundAndResetReady(gameId, nextRound, teamsObj) {
   const updates = {};
-  updates[`games/${gameId}/currentRound`] = nextRound;
+  updates[`games/decrypto/${gameId}/currentRound`] = nextRound;
   Object.keys(teamsObj||{}).forEach(t => {
-    updates[`games/${gameId}/teams/${t}/ready`] = false;
+    updates[`games/decrypto/${gameId}/teams/${t}/ready`] = false;
   });
   await update(ref(db), updates);
 }
